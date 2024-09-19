31f5b00c0202217d26ce4910f1bc0da3117c84ed
Anmol Jain <anmolj678@gmail.com>
2024-09-18 22:47:55 -0700

    init

---
+++ api/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[os.getenv("WEBAPP_URL")],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
