0919d73fa792cbc001c975a5152e9c75696aec5c
Anmol Jain <anmolj678@gmail.com>
2024-09-21 01:45:08 -0700

    add new types

--- web/src/types.ts
+++ web/src/types.ts
@@ -1,5 +1,24 @@
-
 export interface Table {
   name: string
   schema: string
+}
+
+interface Selectable {
+  isSelected: boolean
+}
+
+export interface TableSelectable extends Table, Selectable {}
+
+export interface Message {
+  id: string
+  content: string
+  sender: 'user' | 'bot'
+}
+
+export interface AppState {
+  tables: TableSelectable[]
+  dataGenerationScript: string
+  messages: Message[]
+  loading: boolean
+  error: string | null
 }
