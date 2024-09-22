0748c185465e15e0e4c39e3f5583e5a5c7683cf5
Anmol Jain <anmolj678@gmail.com>
2024-09-21 23:05:01 -0700

    tab view styles

--- web/src/components/TabsView.tsx
+++ web/src/components/TabsView.tsx
@@ -2,11 +2,14 @@ import { Tab } from '@/types'
 
 export default function TabsView({ tabs, activeTab, setActiveTab }: { tabs: Tab[]; activeTab: Tab; setActiveTab: (tab: Tab) => void }) {
   return (
-    <div className="flex border-b-2">
+    <div className="flex border-b-2 px-4">
       {tabs.map((tab) => (
         <button
           key={tab.name}
-          className={`px-4 py-2 font-semibold ${activeTab === tab ? 'border-b-2 border-blue-500' : 'text-gray-500'}`}
+          className={`px-4 py-2 font-semibold -mb-0.5 ${activeTab === tab
+            ? 'border-b-2 border-blue-500 text-blue-500'
+            : 'text-gray-500 hover:text-gray-700'
+            }`}
           onClick={() => setActiveTab(tab)}
         >
           {tab.name}
